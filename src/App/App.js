d868177fdbc99a0a08e0924b97c25988c957f344
Tanner <tanner.weir15@gmail.com>
2023-06-21 16:39:08 -0700

    Create Static Component Folders/Files

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
+import About from "../Components/About/About";
+import Contact from "../Components/Contact/Contact";
+import Featured from "../Components/Featured/Featured";
+import Footer from "../Components/Footer/Footer";
+import Header from "../Components/Header/Header";
+import Home from "../Components/Home/Home";
+import Products from "../Components/Products/Products";
+
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
