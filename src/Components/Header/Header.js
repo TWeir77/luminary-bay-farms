f94c88c03b5386fd34c97dffc3ac938b53758bcd
turbo <tanner.weir15@gmail.com>
2023-07-20 15:48:27 -0700

    Add Base Header CSS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,22 +5,26 @@ import './Header.css';
 export default function Header() {
   return (
     <div className='header'>
-      <NavLink
-        to='/'
-        className='home-icon'
-      > Luminary Bay Farms </NavLink>
-      <NavLink
-        to="products"
-        className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
-      > Micro Greens </NavLink>
-      <NavLink
-        to="about"
-        className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
-      > About </NavLink>
-      <NavLink
-        to="contact"
-        className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
-      > Contact </NavLink>
+      <div className="home-button">
+        <NavLink
+          to='/'
+          className='home-icon'
+        > Luminary Bay <br />Farms </NavLink>
+      </div>
+      <div className="nav">
+        <NavLink
+          to="products"
+          className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
+        > Micro Greens </NavLink>
+        <NavLink
+          to="about"
+          className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
+        > About </NavLink>
+        <NavLink
+          to="contact"
+          className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
+        > Contact </NavLink>
+      </div>
     </div>
   )
 }
