b612e5d201bebe7ff90a02ecbbdebc88e3182ea7
turbo <tanner.weir15@gmail.com>
2023-07-20 16:25:16 -0700

    Add Footer CSS

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,10 +6,11 @@ export default function Footer() {
   return (
     <footer>
       <ul>
-        <li><Link to='about'>About</Link></li>
-        <li><Link to='products'>Micro Greens</Link></li>
-        <li><Link to='contact'>Contact</Link></li>
+        <li><Link className='footer-link' to='about'>About</Link></li>
+        <li><Link className='footer-link' to='products'>Micro Greens</Link></li>
+        <li><Link className='footer-link' to='contact'>Contact</Link></li>
       </ul>
+      <p>© 2023 All rights Reserved. Design by Tanner Weir</p>
     </footer>
   )
 }
