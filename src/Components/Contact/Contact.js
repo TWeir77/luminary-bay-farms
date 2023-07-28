1087b72fef6c2e5c80ad62307840fab1db7b664a
Tanner <tanner.weir15@gmail.com>
2023-07-27 19:09:00 -0700

    complete contact form and design

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,17 +4,37 @@ import './Contact.css';
 export default function Contact() {
   return (
     <main>
-      <div>
-        <h1>Let's Chat!</h1>
+      <div id="contact-title">
+        <h1 className="section-title">Let's Chat!</h1>
       </div>
-      <form action="mailto:example@gmail.com">
+      <form id="form" action="mailto:example@gmail.com">
+        <h4 id='form-header'>Reach out to us with any inquiries regarding our microgreens!</h4>
+        <p id="form-subheader">We're happy to tell you where to get them, what's coming soon, and recommendations for new recipe ideas. Thanks for choosing Luminary Bay Farms.</p>
         Subject:<br/>
-        <input type="text" name="subject" placeholder="Your Name" required /><br/>
+        <input className="input" type="text" name="subject" placeholder="Your Name" required /><br/>
         Email:<br/>
-        <input type="email" name="email" placeholder="Your Email" required /><br/>
+        <input className="input" type="email" name="email" placeholder="Your Email" required /><br/>
+        Please tell us a little about your inquiry:
+        <div className="radio">
+          <label for='delivery'>Does Luminary Bay Farms deliver to my zip code?</label>
+          <input className="bubble" type="radio" id="delivery" name="inquiry" value="delivery"/>
+        </div>
+        <div className="radio">
+          <label for='learnMore'>I'd like to learn more about microgreens.</label>
+          <input className="bubble" type="radio" id="learnMore" name="inquiry" value="learnMore"/>
+        </div>
+        <div className="radio">
+          <label for='contract'>My restaurant is interested in your microgreens.</label>
+          <input className="bubble" type="radio" id="contract" name="inquiry" value="contract"/>
+        </div>
+        <div className="radio">
+          <label for='other'>Other: please specify below.</label>
+          <input className="bubble" type="radio" id="other" name="inquiry" value="other"/>
+        </div>
+        <br/>
         Message:<br/>
         <textarea name="body" rows="5" cols="30" placeholder="Your Message"></textarea><br/>
-        <input type="submit" value="Send"/>
+        <input id="submit-button" type="submit" value="Send"/>
       </form>
     </main>
   )
