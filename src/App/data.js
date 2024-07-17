58b2ee488f2d2d2fa5d7c92409d576b452244909
Tanner <tanner.weir15@gmail.com>
2024-07-17 11:09:44 -0700

    Add Product, Correct Errors

--- src/App/data.js
+++ src/App/data.js
@@ -1,37 +1,44 @@
 export const PRODUCTS = {
   "sunflower": {
     name: "Sunflower",
-    body: `This microgreen is a personal favorite, and it's easy to imagine why. Sunflower seeds already make for such a great snack, but as microgreens, sunflowers are so full of fresh flavor, it's hard to put them down!\nOn the go, or topping for a meal, these little sunflower sprouts are a great source of protein and essential amino acids. They are also rich in vitamins A-E, and K. Mineral nutrients include calcium, iron, magnesium, phosphorus, copper, zinc, manganese, and selenium.\n\nAverage Gestation Period: 5 days\nAverage Growth After Gestation: 8 days`,
+    body: `This microgreen is a personal favorite, and it's easy to imagine why. Sunflower seeds already make for such a great snack, but as microgreens, sunflowers are so full of fresh flavor, it's hard to put them down!\nOn the go, or topping for a meal, these little sunflower sprouts are a great source of protein and essential amino acids. They are also rich in vitamins A-E, and K. Mineral nutrients include calcium, iron, magnesium, phosphorus, copper, zinc, manganese, and selenium.\n\nAverage Germination Period: 5 days\nAverage Growth After Germination: 8 days`,
     image: '/images/sunflower-seeds.jpg',
     featured: false,
     slug: "sunflower"
   },
   "peas": {
     name: "Peas",
-    body: `Describing the flavor of peas can come with a lot of variety, however, the largest consesus is that peas often have the great combo of being both sweet and savory. In microgreen form, it's quite the same, containing a deliciously sweet taste! This makes them a key favorite for salads, as well as powerful smoothies and juices! \nAs for that nutritional benefit, pea microgreens are absolutely full of vitamins B, C, and K. They are rich in the minerals iron, manganese, and choline. Each of these nutrients in pea microgreens make for the perfect immunity booster, as well as support for heart health, and aid for detoxing.\n\nAverage Gestation Period: 4 days\nAverage Growth After Gestation: 8 days`,
+    body: `Describing the flavor of peas can come with a lot of variety, however, the largest consensus is that peas often have the great combo of being both sweet and savory. In microgreen form, it's quite the same, containing a deliciously sweet taste! This makes them a key favorite for salads, as well as powerful smoothies and juices! \nAs for that nutritional benefit, pea microgreens are absolutely full of vitamins B, C, and K. They are rich in the minerals iron, manganese, and choline. Each of these nutrients in pea microgreens make for the perfect immunity booster, as well as support for heart health, and aid for detoxing.\n\nAverage Germination Period: 4 days\nAverage Growth After Germination: 8 days`,
     image: '/images/pea-seeds.jpg',
     featured: false,
     slug: "peas"
   },
   "broccoli": {
     name: "Broccoli",
-    body: `Another microgreen that takes after it's fully-grown counterpart, broccoli microgreens maintain their mildly bitter, earthy taste as a sprout. Although the taste may not be as mouth-watering as some others, this makes them a great addition to sweeter meals, where it can balance the flavor. Sandwiches and salads love this microgreen!\nThe real bang for your buck with these microgeens comes from the nutrients. Broccoli microgreens are an incredible source of protein, fiber, and potassium. They also include calcium, phosphorous, magnesium, iron, manganese, and zinc. A whopping serving of vitamin A, as well as C and E, round out these microgreens to really kick you into high gear.\n\nAverage Gestation Period: 3 days\nAverage Growth After Gestation: 9 days`,
+    body: `Another microgreen that takes after it's fully-grown counterpart, broccoli microgreens maintain their mildly bitter, earthy taste as a sprout. Although the taste may not be as mouth-watering as some others, this makes them a great addition to sweeter meals, where it can balance the flavor. Sandwiches and salads love this microgreen!\nThe real bang for your buck with these microgreens comes from the nutrients. Broccoli microgreens are an incredible source of protein, fiber, and potassium. They also include calcium, phosphorous, magnesium, iron, manganese, and zinc. A whopping serving of vitamin A, as well as C and E, round out these microgreens to really kick you into high gear.\n\nAverage Germination Period: 3 days\nAverage Growth After Germination: 9 days`,
     image: '/images/broccoli-tray-2.jpg',
     featured: true,
     slug: "broccoli"
   },
   "radish": {
     name: "Radish",
-    body: `These microgreens pack a subtle spice flavor that is rather addicting. Just like the fully grown radish, the flavor is zesty, and crisp with a kick of spice!\nPaired great with a heartier meal or snack, radish sprouts are a great source of vitamins A, B, C, E, and K. Mineral nutrients include calcium, iron, magnesium, phosphorus, potassium, and zinc.\n\nAverage Gestation Period: 3 days\nAverage Growth After Gestation: 7 days`,
+    body: `These microgreens pack a subtle spice flavor that is rather addicting. Just like the fully grown radish, the flavor is zesty, and crisp with a kick of spice!\nPaired great with a heartier meal or snack, radish sprouts are a great source of vitamins A, B, C, E, and K. Mineral nutrients include calcium, iron, magnesium, phosphorus, potassium, and zinc.\n\nAverage Germination Period: 3 days\nAverage Growth After Germination: 7 days`,
     image: '/images/radish-tray-2.jpg',
     featured: true,
     slug: "radish"
   },
   "cabbage": {
     name: "Cabbage",
-    body: `Unlike the full-grown cabbage, the taste of broccoli microgreens is much milder, and less tangy. Instead, these microgreens are far sweeter and have a nice crunch to them. This gives them a great taste for a light snack, of course, there's no reason to stop there!\nTaste isn't the only difference here, in fact, a serving of these microgreens has more vitmain C than an orange, and an astounding 40 times more vitamin E than mature cabbage! Other rich nutrients include fiber, beta-carotene, calcium, magnesium, potassium, iodine, and sulfur. A true powerhouse for your body's health and immunity.\n\nAverage Gestation Period: 2 days\nAverage Growth After Gestation: 7 days`,
+    body: `Unlike the full-grown cabbage, the taste of cabbage microgreens is much milder, and less tangy. Instead, these microgreens are far sweeter and have a nice crunch to them. This gives them a great taste for a light snack, of course, there's no reason to stop there!\nTaste isn't the only difference here, in fact, a serving of these microgreens has more vitamin C than an orange, and an astounding 40 times more vitamin E than mature cabbage! Other rich nutrients include fiber, beta-carotene, calcium, magnesium, potassium, iodine, and sulfur. A true powerhouse for your body's health and immunity.\n\nAverage Germination Period: 2 days\nAverage Growth After Germination: 7 days`,
     image: '/images/cabbage-tray-2.jpg',
     featured: true,
     slug: "cabbage"
+  },
+  "arugula": {
+    name: "Arugula",
+    body: `Arugula microgreens bring a bold, peppery flavor that can transform any dish. Often described as slightly tangy with a hint of spiciness, these microgreens are perfect for adding a zesty kick to salads, sandwiches, and pizzas. But the flavor is just the beginning. Arugula microgreens are a powerhouse of nutrients, rich in vitamins A, B, C, E, and K.\nThey also contain significant amounts of calcium, iron, magnesium, phosphorus, potassium, and zinc. These nutrients support bone health, boost the immune system, and contribute to overall well-being.\n\nAverage Germination Period: 3 days\nAverage Growth After Germination: 6 days.`,
+    image: '/images/cabbage-tray-2.jpg',
+    featured: false,
+    slug: "arugula"
   },
 }
